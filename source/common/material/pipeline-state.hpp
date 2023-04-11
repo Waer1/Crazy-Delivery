953000b176a3ce920707aa0d31bc44cfc90c8034
BeshoyMorad <besh0morta@gmail.com>
2023-04-11 19:58:15 +0200

    Finish pipe-line state

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -42,6 +42,41 @@ namespace our {
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 4) Write this function
+
+						// Check if face culling is enabled then set the culling face and front face
+						if (faceCulling.enabled) {
+								glEnable(GL_CULL_FACE);
+								glCullFace(faceCulling.culledFace);
+								glFrontFace(faceCulling.frontFace);
+						} // else disable face culling
+						else {
+								glDisable(GL_CULL_FACE);
+						}
+
+						// Check if depth testing is enabled then set the depth function
+						// and specify the color and depth mask
+						if (depthTesting.enabled) {
+								glEnable(GL_DEPTH_TEST);
+								glDepthFunc(depthTesting.function);
+
+								glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
+								glDepthMask(depthMask);
+						} // else disable depth testing
+						else {
+								glDisable(GL_DEPTH_TEST);
+						}
+
+						// Check if blending is enabled then set the blending equation, source factor and destination factor
+						// and specify the constant color
+						if (blending.enabled) {
+								glEnable(GL_BLEND);
+								glBlendEquation(blending.equation);
+								glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+								glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+						} // else disable blending
+						else {
+								glDisable(GL_BLEND);
+						}
         }
 
         // Given a json object, this function deserializes a PipelineState structure
