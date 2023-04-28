e8f0e60a3ed4abb7f1f9ecbee5c79da83e170afb
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-28 15:46:33 +0300

    Better comments & formatting for req 4

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -41,58 +41,59 @@ namespace our {
         // This function should set the OpenGL options to the values specified by this structure
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
-            //TODO: (Req 4) Write this function
-						// Check if face culling is enabled then set the culling face and front face
-                        // we know the culling face and front face as we send to it if it's a CCW or CW
-                        // Counter clockwise-> Front Face
-                        // Clockwise-> Back Face
-                        // if we rotated the object 180 degree then the faces will be changed
+            // TODO: (Req 4) Write this function
+            // Check if face culling is enabled then set the culling face and front face
+            // We know the culling face and front face as we send to it if it's a CCW or CW
+            // Counter clockwise-> Front Face
+            // Clockwise-> Back Face
+            // If we rotated the object 180 degree then the faces will be changed
+            if (faceCulling.enabled) {
+                // Firstly we need to enable GL_CULL_FACE
+                glEnable(GL_CULL_FACE);
+                // Which faces we will remove
+                glCullFace(faceCulling.culledFace);
+                // Direction of the front face that won't be removed
+                glFrontFace(faceCulling.frontFace);
+            }
+            else {
+                // Else disable face culling
+                glDisable(GL_CULL_FACE);
+            }
 
-                        // if we want to remove the front face then we will remove the back face
-						if (faceCulling.enabled) {
-                                //firstly we need to enable GL_CULL_FACE
-								glEnable(GL_CULL_FACE);
-                                //which faces we will remove
-								glCullFace(faceCulling.culledFace);
-                                //direction of the front face that won't be removed
-								glFrontFace(faceCulling.frontFace);
-						} // else disable face culling
-						else {
-								glDisable(GL_CULL_FACE);
-						}
+            // Check if depth testing is enabled then set the depth function
+            // and specify the color and depth mask
+            if (depthTesting.enabled) {
+                // Firstly we need to enable GL_DEPTH_TEST
+                glEnable(GL_DEPTH_TEST);
+                // Which method should we choose to make the depth testing
+                glDepthFunc(depthTesting.function);
+                // Set the color & depth mask using the GL functions & simply passing them
+                glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
+                glDepthMask(depthMask);
+            }
+            else {
+                // Else disable depth testing
+                glDisable(GL_DEPTH_TEST);
+            }
 
-						// Check if depth testing is enabled then set the depth function
-						// and specify the color and depth mask
-						if (depthTesting.enabled) {
-                                //firstly we need to enable GL_DEPTH_TEST
-								glEnable(GL_DEPTH_TEST);
-                                //which method should we choose to make the depth testing
-								glDepthFunc(depthTesting.function);
-                                // which color and depth mask we will use
-								glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
-                                // which color and depth mask
-								glDepthMask(depthMask);
-						} // else disable depth testing
-						else {
-								glDisable(GL_DEPTH_TEST);
-						}
-
-						// Check if blending is enabled then set the blending equation, source factor and destination factor
-						// and specify the constant color
-						if (blending.enabled) {
-                                //firstly we need to enable GL_BLEND
-								glEnable(GL_BLEND);
-								glBlendEquation(blending.equation);
-								glBlendFunc(blending.sourceFactor, blending.destinationFactor);
-								glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
-						} // else disable blending
-						else {
-								glDisable(GL_BLEND);
-						}
+            // Check if blending is enabled then set the blending equation, source factor and destination factor
+            // and specify the constant color
+            if (blending.enabled) {
+                // Firstly we need to enable GL_BLEND
+                glEnable(GL_BLEND);
+                // Set the blending equation, source factor and destination factor
+                glBlendEquation(blending.equation);
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+                // Retrieve the constant color from blending and set as our blend color
+                glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+            }
+            else {
+                // Else disable blending
+                glDisable(GL_BLEND);
+            }
         }
 
         // Given a json object, this function deserializes a PipelineState structure
         void deserialize(const nlohmann::json& data);
     };
-
 }
